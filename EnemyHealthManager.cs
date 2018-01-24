b1e0886f8ec4e2ebe1561d8ccdf452fa400158d4
Blacksaw95 <35782232+Blacksaw95@users.noreply.github.com>
2018-01-24 16:35:22 -0500

    Add files via upload

---
+++ EnemyHealthManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthManager : MonoBehaviour {
+
+    public int Maxhealth;
+    public int CurrentHealth;
+
+    // Use this for initialization
+    void Start()
+    {
+        CurrentHealth = Maxhealth;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CurrentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+
+        }
+
+
+    }
+
+    public void HurtEnemy(int damageToGive)
+    {
+        CurrentHealth -= damageToGive;
+    }
+
+    public void SetMaxHealth()
+    {
+        CurrentHealth = Maxhealth;
+    }
+
+}
---
+++ SlimeController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SlimeController : MonoBehaviour {
+
+    public float moveSpeed;
+    private Rigidbody2D myRigidBody;
+    private bool moving;
+    public float timeBetweenMove;
+    private float timeBetweenMoveCounter;
+    public float timeToMove;
+    private float timeToMoveCounter;
+    private Vector3 moveDirection;
+    public float waitToReload;
+    private bool reloading;
+    private GameObject thePlayer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        //timeBetweenMoveCounter = timeBetweenMove;
+        //timeToMoveCounter = timeToMove;
+
+        timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0f, timeBetweenMove * 3f);
+        timeToMoveCounter = Random.Range(timeToMove * 0f, timeToMove * 3f);
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (moving)
+        {
+            timeToMoveCounter -= Time.deltaTime;
+            myRigidBody.velocity = moveDirection;
+
+            if(timeToMoveCounter < 0f)
+            {
+                moving = false;
+                //timeBetweenMoveCounter = timeBetweenMove;
+                timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0f, timeBetweenMove * 3f);
+            }
+        }
+        else
+        
+            {
+            timeBetweenMoveCounter -= Time.deltaTime;
+            myRigidBody.velocity = Vector2.zero;
+            if (timeBetweenMoveCounter < 0f)
+            {
+                moving = true;
+                //timeToMoveCounter = timeToMove;
+                timeToMoveCounter = Random.Range(timeToMove * 0f, timeToMove * 3f);
+                moveDirection = new Vector3(Random.Range(-1f, 1f) * moveSpeed, Random.Range(-1f, 1f) * moveSpeed, 0f);
+            }
+            }
+        if(reloading)
+        {
+            waitToReload -= Time.deltaTime;
+            if(waitToReload < 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                thePlayer.SetActive(true);
+            }
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        /* if(other.gameObject.name == "Player")
+        {
+            //Destroy(other.gameObject);
+            other.gameObject.SetActive(false);
+            reloading = true;
+            thePlayer = other.gameObject;
+        } */
+    }
+
+}
