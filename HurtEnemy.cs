8d1b613669285340fb041b9e9d42a92e2b6ed11d
Blacksaw95 <35782232+Blacksaw95@users.noreply.github.com>
2018-01-26 10:15:07 -0500

    Update HurtEnemy.cs

--- HurtEnemy.cs
+++ HurtEnemy.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HurtEnemy : MonoBehaviour {
 
     public int damageToGive;
+    private int currentDamage;
     public GameObject damageBurst;
     public Transform hitPoint;
     public GameObject damageNumber;
 
+    private PlayerStats thePS;
 
 	// Use this for initialization
 	void Start () {
-		
+
+        thePS = FindObjectOfType<PlayerStats>();
+
 	}
 	
 	// Update is called once per frame
@@ -25,10 +29,12 @@ public class HurtEnemy : MonoBehaviour {
 
         if(other.gameObject.tag == "Enemy")
         {
-            other.gameObject.GetComponent<EnemyHealthManager>().HurtEnemy(damageToGive);
+            currentDamage = damageToGive + thePS.currentAttack;
+
+            other.gameObject.GetComponent<EnemyHealthManager>().HurtEnemy(currentDamage);
             Instantiate(damageBurst, hitPoint.position, hitPoint.rotation);
             var clone = (GameObject)Instantiate(damageNumber, hitPoint.position, Quaternion.Euler(Vector3.zero));
-            clone.GetComponent<FloatingNumbers>().damageNumber = damageToGive;
+            clone.GetComponent<FloatingNumbers>().damageNumber = currentDamage;
             clone.transform.position = new Vector2(transform.position.x, transform.position.y);
 
         }
